0043753306f8c8056eb7b3e474756ad799b90f07
Bobby Towers <sdfwer124@gmail.com>
2017-11-20 18:29:28 -0800

    Add files via upload

---
+++ src/day04.clj
@@ -0,0 +1,76 @@
+(ns day04.core
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as string]
+   [instaparse.core :as insta]))
+
+
+(comment
+  (real-room? (parse-room "aaaaa-bbb-z-y-x-123[abxyz]"))
+  (real-room? (parse-room "a-b-c-d-e-f-g-h-987[abcde]"))
+  (real-room? (parse-room "not-a-real-room-404[oarel]"))
+  (real-room? (parse-room "totally-real-room-200[decoy]"))
+  )
+
+(def room-parser
+  (insta/parser
+    "room = key id checksum
+     key = (word <separator>)+
+     <separator> = '-'
+     word = #'[a-zA-Z]+'
+     checksum = '[' word ']' :end-of-string?
+     whitespace = #'\\s+'
+     id = #'[0-9]+'"))
+
+(def lines (line-seq (io/reader (io/resource "day4"))))
+
+(defn parse-room [s]
+  (let [parts (string/split s #"-")
+        [id chk] (string/split (last parts) #"\[")]
+    {:word  (apply concat (butlast parts))
+     :chksum (butlast chk)
+     :id (Integer/parseInt id)}))
+
+(defn checksum [word]
+  (->> word
+       frequencies
+       (sort-by (fn [[a b]] [(- b) (int a)]))
+       (map first)
+       (take 5)))
+
+(defn real-room? [{:keys [word chksum] :as room}]
+  (= (checksum word) chksum))
+
+
+;; part 1
+#_(->> lines
+       (map parse-room)
+       (filter real-room?)
+       (map :id)
+       (reduce +))
+;;=> 278221
+
+(defn shift-letter [n letter]
+  (-> letter
+      int
+      (- 97)
+      (+ n)
+      (mod 26)
+      (+ 97)
+      char))
+
+(defn decrypt [{:keys [word id] :as room}]
+  (assoc room :decrypted
+         (apply str (map (partial shift-letter id) word))))
+
+#_(decrypt (parse-room "qzmt-zixmtkozy-ivhz-343"))
+
+;; part 2
+#_(->> lines
+       (map parse-room)
+       (filter real-room?)
+       (map decrypt)
+       (filter #(re-matches #".*north.*" (:decrypted %)))
+       first
+       :id)
+;; => 267
