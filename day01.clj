402f0d82d1542a9986497fa52de7a08eaf917b9a
Bobby Towers <sdfwer124@gmail.com>
2017-11-21 23:31:25 -0800

    Add files via upload

--- day01.clj
+++ day01.clj
@@ -1,11 +1,34 @@
-(ns advent.core)
+(ns advent.core
+  (:require [instaparse.core :as insta]))
+
+; Our first task is to rescue Santa's sleigh which was stolen by the Easter bunny. We're dropped off in the middle of a city with an impossibly long list of directions and to reach the goal we have to calculate the "Manhattan distance", the shortest path along a grid,  which is equal to the sum of the absolute differences of the Cartesian coordinates. That's really just a fancy way of asking, "How far is it in each direction?"
+
+; So I made that function:
+
+(defn bunny [u v]
+  (reduce +
+    (map (fn [[a b]] (Math/abs (- a b)))
+         (map vector u v))))
+
+; But first we have to parse the directions into a sequence of steps and turns, and then appropriately incremented or decremented my coordinates according to the direction of travel, determined by dividing the value of the number of turns by 4 and taking the remainder.
 
 ; Start at the given coordinates (where you just landed) and face North.
 
 ; Represent our location as cartesian coordinates
 ; follow the provided sequence:
 
-(def steps '(R2, L3, R2, R4, L2, L1, R2, R4, R1, L4, L5, R5, R5, R2, R2, R1, L2, L3, L2, L1, R3, L5, R187, R1, R4, L1, R5, L3, L4, R50, L4, R2, R70, L3, L2, R4, R3, R194, L3, L4, L4, L3, L4, R4, R5, L1, L5, L4, R1, L2, R4, L5, L3, R4, L5, L5, R5, R3, R5, L2, L4, R4, L1, R3, R1, L1, L2, R2, R2, L3, R3, R2, R5, R2, R5, L3, R2, L5, R1, R2, R2, L4, L5, L1, L4, R4, R3, R1, R2, L1, L2, R4, R5, L2, R3, L4, L5, L5, L4, R4, L2, R1, R1, L2, L3, L2, R2, L4, R3, R2, L1, L3, L2, L4, L4, R2, L3, L3, R2, L4, L3, R4, R3, L2, L1, L4, R4, R2, L4, L4, L5, L1, R2, L5, L2, L3, R2, L2))
+(def steps "R2, L3, R2, R4, L2, L1, R2, R4, R1, L4, L5, R5, R5, R2, R2, R1, L2, L3, L2, L1, R3, L5, R187, R1, R4, L1, R5, L3, L4, R50, L4, R2, R70, L3, L2, R4, R3, R194, L3, L4, L4, L3, L4, R4, R5, L1, L5, L4, R1, L2, R4, L5, L3, R4, L5, L5, R5, R3, R5, L2, L4, R4, L1, R3, R1, L1, L2, R2, R2, L3, R3, R2, R5, R2, R5, L3, R2, L5, R1, R2, R2, L4, L5, L1, L4, R4, R3, R1, R2, L1, L2, R4, R5, L2, R3, L4, L5, L5, L4, R4, L2, R1, R1, L2, L3, L2, R2, L4, R3, R2, L1, L3, L2, L4, L4, R2, L3, L3, R2, L4, L3, R4, R3, L2, L1, L4, R4, R2, L4, L4, L5, L1, R2, L5, L2, L3, R2, L2")
+
+(def step-parser
+  (insta/parser
+    "step = (direction steps)+
+     direction = #'[a-zA-Z]+'
+     steps = #'[0-9]+' ', '?"))
+
+; This produces the following data structure:
+
+(step-parser steps)
+;=> [:step [:direction "R"] [:steps "2" ", "] [:direction "L"] [:steps "3" ", "] [:direction "R"] [:steps "2" ", "] [:direction "R"] [:steps "4" ", "] [:direction "L"] [:steps "2" ", "] [:direction "L"] [:steps "1" ", "] [:direction "R"] [:steps "2" ", "] [:direction "R"] [:steps "4" ", "] [:direction "R"] [:steps "1" ", "] [:direction "L"] [:steps "4" ", "] [:direction "L"] [:steps "5" ", "] [:direction "R"] [:steps "5" ", "] [:direction "R"] [:steps "5" ", "] [:direction "R"] [:steps "2" ", "] [:direction "R"] [:steps "2" ", "] [:direction "R"] [:steps "1" ", "] [:direction "L"] [:steps "2" ", "] [:direction "L"] [:steps "3" ", "] [:direction "L"] [:steps "2" ", "] [:direction "L"] [:steps "1" ", "] [:direction "R"] [:steps "3" ", "] [:direction "L"] [:steps "5" ", "] [:direction "R"] [:steps "187" ", "] [:direction "R"] [:steps "1" ", "] [:direction "R"] [:steps "4" ", "] [:direction "L"] [:steps "1" ", "] [:direction "R"] [:steps "5" ", "] [:direction "L"] [:steps "3" ", "] [:direction "L"] [:steps "4" ", "] [:direction "R"] [:steps "50" ", "] [:direction "L"] [:steps "4" ", "] [:direction "R"] [:steps "2" ", "] [:direction "R"] [:steps "70" ", "] [:direction "L"] [:steps "3" ", "] [:direction "L"] [:steps "2" ", "] [:direction "R"] [:steps "4" ", "] [:direction "R"] [:steps "3" ", "] [:direction "R"] [:steps "194" ", "] [:direction "L"] [:steps "3" ", "] [:direction "L"] [:steps "4" ", "] [:direction "L"] [:steps "4" ", "] [:direction "L"] [:steps "3" ", "] [:direction "L"] [:steps "4" ", "] [:direction "R"] [:steps "4" ", "] [:direction "R"] [:steps "5" ", "] [:direction "L"] [:steps "1" ", "] [:direction "L"] [:steps "5" ", "] [:direction "L"] [:steps "4" ", "] [:direction "R"] [:steps "1" ", "] [:direction "L"] [:steps "2" ", "] [:direction "R"] [:steps "4" ", "] [:direction "L"] [:steps "5" ", "] [:direction "L"] [:steps "3" ", "] [:direction "R"] [:steps "4" ", "] [:direction "L"] [:steps "5" ", "] [:direction "L"] [:steps "5" ", "] [:direction "R"] [:steps "5" ", "] [:direction "R"] [:steps "3" ", "] [:direction "R"] [:steps "5" ", "] [:direction "L"] [:steps "2" ", "] [:direction "L"] [:steps "4" ", "] [:direction "R"] [:steps "4" ", "] [:direction "L"] [:steps "1" ", "] [:direction "R"] [:steps "3" ", "] [:direction "R"] [:steps "1" ", "] [:direction "L"] [:steps "1" ", "] [:direction "L"] [:steps "2" ", "] [:direction "R"] [:steps "2" ", "] [:direction "R"] [:steps "2" ", "] [:direction "L"] [:steps "3" ", "] [:direction "R"] [:steps "3" ", "] [:direction "R"] [:steps "2" ", "] [:direction "R"] [:steps "5" ", "] [:direction "R"] [:steps "2" ", "] [:direction "R"] [:steps "5" ", "] [:direction "L"] [:steps "3" ", "] [:direction "R"] [:steps "2" ", "] [:direction "L"] [:steps "5" ", "] [:direction "R"] [:steps "1" ", "] [:direction "R"] [:steps "2" ", "] [:direction "R"] [:steps "2" ", "] [:direction "L"] [:steps "4" ", "] [:direction "L"] [:steps "5" ", "] [:direction "L"] [:steps "1" ", "] [:direction "L"] [:steps "4" ", "] [:direction "R"] [:steps "4" ", "] [:direction "R"] [:steps "3" ", "] [:direction "R"] [:steps "1" ", "] [:direction "R"] [:steps "2" ", "] [:direction "L"] [:steps "1" ", "] [:direction "L"] [:steps "2" ", "] [:direction "R"] [:steps "4" ", "] [:direction "R"] [:steps "5" ", "] [:direction "L"] [:steps "2" ", "] [:direction "R"] [:steps "3" ", "] [:direction "L"] [:steps "4" ", "] [:direction "L"] [:steps "5" ", "] [:direction "L"] [:steps "5" ", "] [:direction "L"] [:steps "4" ", "] [:direction "R"] [:steps "4" ", "] [:direction "L"] [:steps "2" ", "] [:direction "R"] [:steps "1" ", "] [:direction "R"] [:steps "1" ", "] [:direction "L"] [:steps "2" ", "] [:direction "L"] [:steps "3" ", "] [:direction "L"] [:steps "2" ", "] [:direction "R"] [:steps "2" ", "] [:direction "L"] [:steps "4" ", "] [:direction "R"] [:steps "3" ", "] [:direction "R"] [:steps "2" ", "] [:direction "L"] [:steps "1" ", "] [:direction "L"] [:steps "3" ", "] [:direction "L"] [:steps "2" ", "] [:direction "L"] [:steps "4" ", "] [:direction "L"] [:steps "4" ", "] [:direction "R"] [:steps "2" ", "] [:direction "L"] [:steps "3" ", "] [:direction "L"] [:steps "3" ", "] [:direction "R"] [:steps "2" ", "] [:direction "L"] [:steps "4" ", "] [:direction "L"] [:steps "3" ", "] [:direction "R"] [:steps "4" ", "] [:direction "R"] [:steps "3" ", "] [:direction "L"] [:steps "2" ", "] [:direction "L"] [:steps "1" ", "] [:direction "L"] [:steps "4" ", "] [:direction "R"] [:steps "4" ", "] [:direction "R"] [:steps "2" ", "] [:direction "L"] [:steps "4" ", "] [:direction "L"] [:steps "4" ", "] [:direction "L"] [:steps "5" ", "] [:direction "L"] [:steps "1" ", "] [:direction "R"] [:steps "2" ", "] [:direction "L"] [:steps "5" ", "] [:direction "L"] [:steps "2" ", "] [:direction "L"] [:steps "3" ", "] [:direction "R"] [:steps "2" ", "] [:direction "L"] [:steps "2"]]
 
 ; as we walk the seq, we change direction depending on the first item, and change the appropriate coordinate based on the second item, the number.
 ; We start facing north. this will be 1.
@@ -19,26 +42,6 @@
 ; 4 - west, decrement x
 
 ; This will return our new coordinates, which we can pass with [0 0] to our manhattan distance function below.  
-
-
-(defn parse-data [d]
-  (->> d
-       (map name)
-       (map (juxt first
-                  #(Integer/parseInt (apply str (rest %)))))))
-
-(parse-data steps)
-;=> ([\R 2] [\L 3] [\R 2] [\R 4] [\L 2] [\L 1] [\R 2] [\R 4] [\R 1] [\L 4] [\L 5] [\R 5] [\R 5] [\R 2] [\R 2] [\R 1] [\L 2] [\L 3] [\L 2] [\L 1] [\R 3] [\L 5] [\R 187] [\R 1] [\R 4] [\L 1] [\R 5] [\L 3] [\L 4] [\R 50] [\L 4] [\R 2] [\R 70] [\L 3] [\L 2] [\R 4] [\R 3] [\R 194] [\L 3] [\L 4] [\L 4] [\L 3] [\L 4] [\R 4] [\R 5] [\L 1] [\L 5] [\L 4] [\R 1] [\L 2] [\R 4] [\L 5] [\L 3] [\R 4] [\L 5] [\L 5] [\R 5] [\R 3] [\R 5] [\L 2] [\L 4] [\R 4] [\L 1] [\R 3] [\R 1] [\L 1] [\L 2] [\R 2] [\R 2] [\L 3] [\R 3] [\R 2] [\R 5] [\R 2] [\R 5] [\L 3] [\R 2] [\L 5] [\R 1] [\R 2] [\R 2] [\L 4] [\L 5] [\L 1] [\L 4] [\R 4] [\R 3] [\R 1] [\R 2] [\L 1] [\L 2] [\R 4] [\R 5] [\L 2] [\R 3] [\L 4] [\L 5] [\L 5] [\L 4] [\R 4] [\L 2] [\R 1] [\R 1] [\L 2] [\L 3] [\L 2] [\R 2] [\L 4] [\R 3] [\R 2] [\L 1] [\L 3] [\L 2] [\L 4] [\L 4] [\R 2] [\L 3] [\L 3] [\R 2] [\L 4] [\L 3] [\R 4] [\R 3] [\L 2] [\L 1] [\L 4] [\R 4] [\R 2] [\L 4] [\L 4] [\L 5] [\L 1] [\R 2] [\L 5] [\L 2] [\L 3] [\R 2] [\L 2])
-
-
-(defn positions [d]
-  (->> (map first d)
-       (reductions #(({\L dec \R inc} %2) %1) 0)
-       rest
-       (map #(mod % 4))
-       (map [[0 1] [1 0] [0 -1] [-1 0]])
-       (mapcat repeat (map second d))
-(reductions (partial map +) (list 0 0))))
 
 ; then we take the last item, our final set of coordinates, which is:
 
