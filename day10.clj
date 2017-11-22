fe26aa63969b83fb550eb213723d0dc389689252
Bobby Towers <sdfwer124@gmail.com>
2017-11-21 19:41:01 -0800

    Update day10.clj

--- day10.clj
+++ day10.clj
@@ -2,17 +2,7 @@
   (:require
    [clojure.java.io :as io]))
 
-; Just start with creating a parser for this:
-
-(def bots "bot 147 gives low to bot 67 and high to bot 71")
-
-(def bot-parser
-  (insta/parser
-    "handoff = bot+
-     bot = 'bot ' id <word*>
-     word = #'[a-zA-Z]+' whitespace
-     whitespace = #'\\s+'
-     id = #'[0-9]+' whitespace?"))
+; The input data consists of instructions like this:
 
 bot 147 gives low to bot 67 and high to bot 71
 bot 142 gives low to bot 128 and high to bot 164
@@ -21,3 +11,22 @@ bot 107 gives low to bot 194 and high to bot 103
 bot 102 gives low to bot 82 and high to bot 3
 bot 101 gives low to bot 46 and high to bot 111
 value 23 goes to bot 76
+We'll break it down to the first line:
+
+(def bots "bot 147 gives low to bot 67 and high to bot 71")
+
+; Now we'll express the relevant data in EBNF notation:
+
+(def bot-parser
+  (insta/parser
+    "handoff = bot+
+     bot = word whitespace id
+     word = #'[a-zA-Z]+'
+     whitespace = #'\\s+'
+     id = #'[0-9]+'"))
+
+; Each transaction performed by the bots is represented as a "handoff". A handoff is a messenger bot followed by one or more receiver bots.
+
+; Let's use this parser to visualize this transaction:
+
+(insta/visualize (bot-parser bots))
